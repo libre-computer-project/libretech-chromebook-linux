94034aa5d5a1ce6a97f6b06ae4b21a8b49de46c8
Da Xue <da@libre.computer>
2024-04-18 19:32:07 -0400

    setup: fix build-essential typo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo apt install mmdebstrap depthcharge-tools build-essentials bison flex libncurses-dev rsync rdfind
+sudo apt -y install eatmydata
+sudo eatmydata apt -y install git mmdebstrap depthcharge-tools build-essential bison flex libncurses-dev libssl-dev rsync rdfind
 git clone --single-branch --depth=1 https://github.com/torvalds/linux.git
 git clone --single-branch --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
